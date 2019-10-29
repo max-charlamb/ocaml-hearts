b50d478b41ae9f81bc950fbdf7bd691790826087
Max Charlamb <mbc222@cornell.edu>
2019-10-29 19:44:32 -0400

    now builds

--- command.ml
+++ command.ml
@@ -1,59 +1,59 @@
-module Command = struct 
-  type index = int
+type index = int
 
-  type card_triple = index * index * index 
+type card_triple = index * index * index 
 
-  type command = 
-    | Quit
-    | Pass of card_triple
-    | Play of index
-    | Help
-    | Restart
+type command = 
+  | Quit
+  | Pass of card_triple
+  | Play of index
+  | Help
+  | Restart
+  | Score
 
-  exception Empty
+exception Empty
 
-  exception Malformed
+exception Malformed
 
-  exception InvalidIndex
+exception InvalidIndex
 
-  let get_first = function 
-    | [] -> raise Empty
-    | h :: t -> h
+let get_first = function 
+  | [] -> raise Empty
+  | h :: t -> h
 
-  let get_rest = function 
-    | [] -> raise Empty
-    | _ :: t -> t
+let get_rest = function 
+  | [] -> raise Empty
+  | _ :: t -> t
 
-  let rec get_words acc = function
-    | [] -> List.rev acc
-    | h :: t -> if h = "" then get_words acc t else get_words (h::acc) t
+let rec get_words acc = function
+  | [] -> List.rev acc
+  | h :: t -> if h = "" then get_words acc t else get_words (h::acc) t
 
-  let get_index ind = 
-    match int_of_string ind with 
-    | exception Failure _ -> raise Malformed
-    | _ -> int_of_string ind
+let get_index ind = 
+  match int_of_string ind with 
+  | exception Failure _ -> raise Malformed
+  | _ -> int_of_string ind
 
-  let make_play rest = 
-    match get_words [] rest with 
-    | h :: [] -> Play (get_index h)
-    | _ -> raise Malformed
+let make_play rest = 
+  match get_words [] rest with 
+  | h :: [] -> Play (get_index h)
+  | _ -> raise Malformed
 
-  let make_pass rest = 
-    match get_words [] rest with 
-    | a :: b :: c :: [] -> Pass (get_index a, get_index b, get_index c)
-    | _ -> raise Malformed
+let make_pass rest = 
+  match get_words [] rest with 
+  | a :: b :: c :: [] -> Pass (get_index a, get_index b, get_index c)
+  | _ -> raise Malformed
 
-  let make_command rest = function
-    | "quit" -> Quit
-    | "help" -> Help
-    | "restart" -> Restart
-    | "pass" -> make_pass rest
-    | "play" -> make_play rest
-    | _ -> raise Malformed
+let make_command rest = function
+  | "quit" -> Quit
+  | "help" -> Help
+  | "restart" -> Restart
+  | "score" -> Score
+  | "pass" -> make_pass rest
+  | "play" -> make_play rest
+  | _ -> raise Malformed
 
-  let parse s = 
-    let lst = String.trim s |> String.split_on_char ' ' in
-    let first = get_first lst in 
-    let rest = get_rest lst in 
-    make_command rest first
-end ke_command rest first
+let parse s = 
+  let lst = String.trim s |> String.split_on_char ' ' in
+  let first = get_first lst in 
+  let rest = get_rest lst in 
+  make_command rest first
