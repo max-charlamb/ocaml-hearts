c8a6f087ff7c5e66845b30a29f2273e978299488
Max Charlamb <mbc222@cornell.edu>
2019-11-04 13:53:18 -0500

    made very basic bot

--- bot.ml
+++ bot.ml
@@ -1,15 +1,66 @@
 open Partialdeck
 open Card
+open Round
+
+module type BotSig = sig 
+
+  val play : Round.t -> int -> card
+  val lead : Round.t -> int -> card
+
+end
 
 module Bot = struct
 
-  let pass state = 
-    Command.Pass (1,2,3)
+  let play state id = 
+    let hand = Round.bot_hand state id in
+    let pile = Round.bot_pile state in
+    let pile_suite = (List.nth pile ((List.length pile) - 1) |> fst).suite in
+    match PartialDeck.lowest hand pile_suite with
+    | exception CardNotFound ->
+      begin
+        match PartialDeck.lowest hand Club with
+        | exception CardNotFound ->
+          begin 
+            match PartialDeck.lowest hand Diamond with
+            | exception CardNotFound ->
+              begin 
+                match PartialDeck.lowest hand Spade with
+                | exception CardNotFound ->
+                  begin 
+                    match PartialDeck.lowest hand Heart with
+                    | exception CardNotFound ->
+                      failwith "Hand has no cards"
+                    | c -> c
+                  end
+                | c -> c
+              end
+            | c -> c
+          end
+        | c -> c
+      end 
+    | c -> c
 
-  let play state = 
-    {suite=Heart;rank=King}
 
-  let lead state = 
-    {suite=Heart;rank=King}
+  let lead state id = 
+    let hand = Round.bot_hand state id in
+    match PartialDeck.lowest hand Club with
+    | exception CardNotFound ->
+      begin 
+        match PartialDeck.lowest hand Diamond with
+        | exception CardNotFound ->
+          begin 
+            match PartialDeck.lowest hand Spade with
+            | exception CardNotFound ->
+              begin 
+                match PartialDeck.lowest hand Heart with
+                | exception CardNotFound ->
+                  failwith "Hand has no cards"
+                | c -> c
+              end
+            | c -> c
+          end
+        | c -> c
+      end
+    | c -> c
 
 end
