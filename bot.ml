9c7403067499498603ff7d80b246c3bb20576e63
Ryan Ali <ra536@cornell.edu>
2019-11-19 16:25:08 -0500

    bots detect queen of spades

--- bot.ml
+++ bot.ml
@@ -82,11 +82,35 @@ module Bot:BotSig = struct
     | Spade -> play_med_helper hand pile qspade qspadetable
     | Heart -> play_easy hand pile
 
+  and play_hard_helper hand pile queen_played queen_table = 
+    match queen_played, queen_table with 
+    | true, false -> play_highest hand pile
+    | true, true
+    | false, true -> play_easy hand pile
+    | false, false -> if List.length pile = 3 then play_highest hand pile 
+      else play_easy hand pile
+
+
+  and play_hard hand pile qspade qspadetable =
+    let pile_suite = (fst (pile |> List.rev |> List.hd)).suite in 
+    match pile_suite with 
+    | Club -> play_easy hand pile 
+    | Diamond -> play_easy hand pile
+    | Spade -> play_med_helper hand pile qspade qspadetable
+    | Heart -> play_easy hand pile
+
+  and list_to_deck pile acc =   
+    match pile with 
+    | (card, _) :: t -> list_to_deck t (PartialDeck.insert card acc)
+    | [] -> acc
 
   and play hand pile diff =
+    let pile' = list_to_deck pile (PartialDeck.empty) in 
+    let qs = {suite = Spade; rank = Queen} in 
+    let qspadetable = (PartialDeck.mem qs pile') in 
     match diff with 
     | "easy" -> play_easy hand pile
-    | "medium" -> play_med hand pile true true
+    | "medium" -> play_med hand pile true qspadetable
     | "hard" -> play_easy hand pile 
     | _ -> play_easy hand pile
 
