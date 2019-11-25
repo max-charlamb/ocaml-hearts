f18c57a5a57e5df2feaf94d15e52c305a5d11e7f
Max Charlamb <mbc222@cornell.edu>
2019-11-25 15:15:10 -0500

    changed format of pass function

--- bot.mli
+++ bot.mli
@@ -5,7 +5,7 @@ module type BotSig = sig
 
   val play : PartialDeck.t -> (card * int) list -> string -> card
   val lead : PartialDeck.t -> (card * int) list -> string -> card
-  val pass : PartialDeck.t -> (card * int) list -> card list
+  val pass : PartialDeck.t -> card list
 
 end
 
