59a5d7e3c00ff18b539a335992e5dd54d89bf4a4
Ryan Ali <ra536@cornell.edu>
2019-11-30 18:40:10 -0500

    all internal documentation for command

--- command.ml
+++ command.ml
@@ -19,38 +19,58 @@ exception Malformed
 
 exception InvalidIndex
 
+(** [get_first lst] is the first word in [lst]. If [lst] is empty, raises
+    Empty exception.  *)
 let get_first = function 
   | [] -> raise Empty
   | h :: t -> if h = "" then raise Empty else h
 
+(** [get_rest lst] is the [lst] with the first word removed. If [lst] is
+    empty, raises Empty exception. *)
 let get_rest = function 
   | [] -> raise Empty
   | _ :: t -> t
 
+(** [get_words acc lst] is all the words in [lst], which does not include
+    the empty string. *)
 let rec get_words acc = function
   | [] -> List.rev acc
   | h :: t -> if h = "" then get_words acc t else get_words (h::acc) t
 
+(** [get_index ind] is the integer represented by [ind]. If [ind] does not
+    represent a valid integer, then raises Malformed. *)
 let get_index ind = 
   match int_of_string ind with 
   | exception Failure _ -> raise Malformed
   | _ -> int_of_string ind
 
+(** [make_play rest] is the Play command with the index of the card to play 
+    in [rest]. If [rest] does not contain one integer index, then raises
+    Malformed. *)
 let make_play rest = 
   match get_words [] rest with 
   | h :: [] -> Play (get_index h)
   | _ -> raise Malformed
 
+(** [make_select rest] is the Select command with the corresponding difficult
+    contained in [rest]. If rest does not contain a single word representing a 
+    difficulty, then raises Malformed.  *)
 let make_select rest = 
   match get_words [] rest with 
   | h :: [] -> Select (h)
   | _ -> raise Malformed
 
+(** [make_pass rest] is the Pass command with the indices of the three cards
+    to pass in [rest]. If [rest] does not contain three valid indices, then 
+    a Malformed exception is raised. *)
 let make_pass rest = 
   match get_words [] rest with 
   | a :: b :: c :: [] -> Pass (get_index a, get_index b, get_index c)
   | _ -> raise Malformed
 
+(** [make_command rest lead] is the Command represented by the lead word 
+    [lead] and the command phrase in [rest]. If these do not correspond to a 
+    valid command, then raises Malformed. *)
 let make_command rest lead = 
   match lead, rest with 
   | "quit", [] -> Quit
