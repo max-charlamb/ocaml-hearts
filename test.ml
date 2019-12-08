4f701e5d58f6d5186b11773894a40dd089910c74
Max Charlamb <mbc222@cornell.edu>
2019-12-08 16:44:21 -0500

    added more test

--- test.ml
+++ test.ml
@@ -12,6 +12,8 @@ open Round
    to pass all the edge cases we can think of. This approach is able to test
    the correctness of the system because the backend is correct, the frontend is 
    easily testable in play testing.
+   We used Bisect to ensure most of our code is covered. We bisect ignored 
+   functions having to do with printing as these are tested visually. 
 *)
 
 (* Card Tests *)
@@ -25,6 +27,8 @@ let cardtests = [
   "compare02" >::(fun _ -> assert_equal (-1) (compare queen_spade king_spade));
   "compare03" >::(fun _ -> assert_equal (-1) (compare queen_heart king_spade));
   "compare04" >::(fun _ -> assert_equal (-1) (compare queen_heart queen_spade));
+  "compare05" >:: (fun _ -> assert_raises DuplicateCard 
+                      (fun () -> compare queen_heart queen_heart));
 ]
 
 (* Partial Deck Tests *)
@@ -59,10 +63,15 @@ let partialdecktests = [
       assert_equal true (PartialDeck.empty |> PartialDeck.is_empty));
   "full" >:: (fun _ ->
       assert_equal 52 (PartialDeck.full |> PartialDeck.size));
-  "insert" >:: (fun _ ->
+  "insert01" >:: (fun _ ->
       assert_equal [({suite=Spade; rank=Queen},1)]
         (PartialDeck.empty |> PartialDeck.insert {suite=Spade; rank=Queen} 
          |> PartialDeck.to_list));
+  "insert02" >:: (fun _ ->
+      assert_raises DuplicateCard
+        (fun () -> PartialDeck.empty |> 
+                   PartialDeck.insert {suite=Spade; rank=Queen} |>
+                   PartialDeck.insert {suite=Spade; rank=Queen} ));
   "move" >:: (fun _ ->
       assert_equal (deck2,deck3) 
         (PartialDeck.move {suite=Spade; rank=King} deck1 PartialDeck.empty));
@@ -91,6 +100,15 @@ let partialdecktests = [
       assert_equal (PartialDeck.shoot_the_moon 
                       (PartialDeck.insert 
                          {suite=Spade; rank=Three} deck6)) true);
+  "remove01" >:: (fun _ -> 
+      assert_raises CardNotFound (fun () -> 
+          deck6 |> PartialDeck.remove {suite=Spade; rank=Three}));
+  "find01" >:: (fun _ -> 
+      assert_equal (Some{rank=Three; suite=Heart}) 
+        (deck6 |> PartialDeck.find 2));
+  "find02" >:: (fun _ -> 
+      assert_equal None 
+        (deck6 |> PartialDeck.find 30));
 ]
 
 (* Command Tests *)
@@ -108,6 +126,12 @@ let commandtests = [
       assert_equal (Restart) (parse "restart"));
   "deal" >:: (fun _ -> 
       assert_equal (Deal) (parse "deal"));
+  "select" >:: (fun _ -> 
+      assert_equal (Select("easy")) (parse "select easy"));
+  "back" >:: (fun _ -> 
+      assert_equal (Back) (parse "back"));
+  "debug" >:: (fun _ -> 
+      assert_equal (Debug) (parse "debug"));
   "empty01" >:: (fun _ -> assert_raises Empty (fun () -> (parse "")));
   "empty02" >:: (fun _ -> assert_raises Empty (fun () -> (parse "  ")));
   "malformed01" >:: (fun _ -> assert_raises Malformed 
@@ -118,21 +142,96 @@ let commandtests = [
                         (fun () -> (parse "play t")));
   "malformed04" >:: (fun _ -> assert_raises Malformed 
                         (fun () -> (parse "quit 324")));
+  "malformed04" >:: (fun _ -> assert_raises Malformed 
+                        (fun () -> (parse "select")));
 ]
+
+(* Round Tests *)
 
 let newround = match Round.new_round Easy |> Round.deal with 
   | Valid t -> t
   | _ -> failwith ""
+
+let testround1 = Round.test_deal [
+    (PartialDeck.add_cards [
+        {rank=Two; suite=Club};
+        {rank=Three; suite=Club};
+        {rank=Four; suite=Club};
+        {rank=Five; suite=Club};
+        {rank=Six; suite=Club};
+        {rank=Seven; suite=Club};
+        {rank=Eight; suite=Club};
+        {rank=Nine; suite=Club};
+        {rank=Ten; suite=Club};
+        {rank=Jack; suite=Club};
+        {rank=Queen; suite=Club};
+        {rank=King; suite=Club};
+        {rank=Ace; suite=Club};
+      ] PartialDeck.empty);
+    (PartialDeck.add_cards [
+        {rank=Two; suite=Heart};
+        {rank=Three; suite=Heart};
+        {rank=Four; suite=Heart};
+        {rank=Five; suite=Heart};
+        {rank=Six; suite=Heart};
+        {rank=Seven; suite=Heart};
+        {rank=Eight; suite=Heart};
+        {rank=Nine; suite=Heart};
+        {rank=Ten; suite=Heart};
+        {rank=Jack; suite=Heart};
+        {rank=Queen; suite=Heart};
+        {rank=King; suite=Heart};
+        {rank=Ace; suite=Heart};
+      ] PartialDeck.empty);
+    (PartialDeck.add_cards [
+        {rank=Two; suite=Spade};
+        {rank=Three; suite=Spade};
+        {rank=Four; suite=Spade};
+        {rank=Five; suite=Spade};
+        {rank=Six; suite=Spade};
+        {rank=Seven; suite=Spade};
+        {rank=Eight; suite=Spade};
+        {rank=Nine; suite=Spade};
+        {rank=Ten; suite=Spade};
+        {rank=Jack; suite=Spade};
+        {rank=Queen; suite=Spade};
+        {rank=King; suite=Spade};
+        {rank=Ace; suite=Spade};
+      ] PartialDeck.empty);
+    (PartialDeck.add_cards [
+        {rank=Two; suite=Diamond};
+        {rank=Three; suite=Diamond};
+        {rank=Four; suite=Diamond};
+        {rank=Five; suite=Diamond};
+        {rank=Six; suite=Diamond};
+        {rank=Seven; suite=Diamond};
+        {rank=Eight; suite=Diamond};
+        {rank=Nine; suite=Diamond};
+        {rank=Ten; suite=Diamond};
+        {rank=Jack; suite=Diamond};
+        {rank=Queen; suite=Diamond};
+        {rank=King; suite=Diamond};
+        {rank=Ace; suite=Diamond};
+      ] PartialDeck.empty);
+  ]
 
 let roundtests = [
   "player hand size" >:: (fun _ -> 
       assert_equal 13 (PartialDeck.size (Round.hand newround)));
   "assert wrong card" >:: (fun _ -> 
       assert_equal false 
-        begin match (Round.play {rank=Ten;suite=Spade} newround) with 
+        begin 
+          match (Round.play {rank=Ten;suite=Spade} newround) with 
           | Invalid(s) -> false
           | Valid (t) -> true
-        end)
+        end);
+  "play 2 clubs first round" >:: (fun _ -> 
+      assert_equal "Play" 
+        begin 
+          match (Round.play {rank=Two;suite=Club} testround1) with 
+          | Invalid(s) -> failwith s
+          | Valid (t) -> t |> Round.next_action
+        end);
 ]
 
 let suite =
